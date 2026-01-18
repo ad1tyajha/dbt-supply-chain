807c2d9cc058497d2a65b26b46a20958d53ff4ee
Aditya Jha <aditya.jha@outlook.in>
2026-01-18 13:01:29 +0000

    Comitting #1

---
+++ models/staging/stg_tpch_orders.sql
@@ -0,0 +1,19 @@
+with source as (
+
+    select * from {{ source('tpch', 'orders') }}
+),
+
+    renamed as (
+
+        select
+        O_ORDERKEY as order_id,
+        O_CUSTKEY as customer_id,
+        O_ORDERSTATUS as order_status,
+        O_TOTALPRICE as total_price,
+        O_ORDERDATE as order_date,
+        O_CLERK as clerk_name
+        from source
+    )
+
+
+select * from renamed
